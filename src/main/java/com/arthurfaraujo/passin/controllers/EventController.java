0acf9702a93ef02371c7c443757365f2cd945c6f
arthur <arthurfelipe5567@gmail.com>
2024-04-03 16:13:24 -0300

    feat: add a new endpoint
    it returns the attendees by the event

--- src/main/java/com/arthurfaraujo/passin/controllers/EventController.java
+++ src/main/java/com/arthurfaraujo/passin/controllers/EventController.java
@@ -9,9 +9,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import com.arthurfaraujo.passin.dto.attendee.AttendeeListResponseDTO;
 import com.arthurfaraujo.passin.dto.event.EventIdDTO;
 import com.arthurfaraujo.passin.dto.event.EventRequestDTO;
 import com.arthurfaraujo.passin.dto.event.EventResponseDTO;
+import com.arthurfaraujo.passin.services.AttendeeService;
 import com.arthurfaraujo.passin.services.EventService;
 
 import lombok.RequiredArgsConstructor;
@@ -21,18 +23,27 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class EventController {
 
-  private final EventService service;
+  private final EventService eventService;
+  private final AttendeeService attendeeService;
 
   @GetMapping("/{id}")
   public ResponseEntity<EventResponseDTO> getEvent(@PathVariable String id) {
-    EventResponseDTO event = this.service.getEventDetail(id);
+    EventResponseDTO event = this.eventService.getEventDetail(id);
 
     return ResponseEntity.ok(event);
   }
+
+  @GetMapping("/{id}/attendees")
+  public ResponseEntity<AttendeeListResponseDTO> getEventAttendees(@PathVariable String id) {
+    AttendeeListResponseDTO attendeeList = this.attendeeService.getAllAttendeesDetailsByEvent(id);
+
+    return ResponseEntity.ok(attendeeList);
+  }
+  
   
   @PostMapping
   public ResponseEntity<EventIdDTO> createEvent(@RequestBody EventRequestDTO body, UriComponentsBuilder uriComponentsBuilder) {
-    EventIdDTO eventIdDTO = this.service.createEvent(body);
+    EventIdDTO eventIdDTO = this.eventService.createEvent(body);
 
     var uri = uriComponentsBuilder.path("/events/{id}").buildAndExpand(eventIdDTO.eventId()).toUri();
     
